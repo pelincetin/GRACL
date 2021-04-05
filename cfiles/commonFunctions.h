555db0bd72da286d775566e21602309bf1363d4c
Maya <mv2731@columbia.edu>
2021-04-04 21:50:24 -0400

    ctypes: Make common functions.h

--- cfiles/commonFunctions.h
+++ cfiles/commonFunctions.h
@@ -1,59 +1,23 @@
+#ifndef COMMON_FUNCTIONS_H
+#define COMMON_FUNCTIONS_H
+
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lockedobject.h"
 
-bool nodeEquals(struct Node* node1, struct Node* node2)
-{
-    if (node1->id == node2->id) {
-        return true;
-    }
-    return false;
-}
+bool nodeEquals(struct Node* node1, struct Node* node2);
 
-bool edgeEquals(struct Edge* e1, struct Edge* e2) {
-    return (((e1->weight == e2->weight) && nodeEquals(e1->start, e2->start)) && nodeEquals(e1->end, e2->end));
-}
+bool edgeEquals(struct Edge* e1, struct Edge* e2);
 
-bool empty_NL(struct NodeList* node_list) {
-    struct NodeListItem* current;
-    current = node_list->head;
-    return (current == NULL);
-}
+bool empty_NL(struct NodeList* node_list);
 
-bool empty_EL(struct EdgeList* edge_list) {
-    struct EdgeListItem *current;
-    current = edge_list->head;
-    return (current == NULL);
-}
+bool empty_EL(struct EdgeList* edge_list);
 
-int length_EL(struct EdgeList* edge_list) {
-    int length = 0;
-    struct EdgeListItem *current;
-    current = edge_list->head;
-    while (current != NULL) {
-        length++;
-        current = current->next;
-    }
-    return length;
-}
+int length_EL(struct EdgeList* edge_list);
 
-int length_NL(struct NodeList* node_list) {
-    int length = 0;
-    struct NodeListItem *current;
-    current = node_list->head;
-    while (current != NULL) {
-        length++;
-        current = current->next;
-    }
-    return length;
-}
+int length_NL(struct NodeList* node_list);
 
-struct NodeListItem* createNodeListItem(struct Node* e) {
-    struct NodeListItem* item = malloc(sizeof(struct NodeListItem));
-    item->node = e;
-    item->next = NULL;
-    item->prev = NULL;
-    return item;
-}
+struct NodeListItem* createNodeListItem(struct Node* e);
 
+#endif
