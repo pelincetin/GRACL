7bdab4aec0cabcf83a62ddf6a2ef918c26b1d662
pelincetin <pc2807@barnard.edu>
2021-04-25 23:41:26 -0400

    clean up conc

--- cfiles/concurrency.c
+++ cfiles/concurrency.c
@@ -46,45 +46,6 @@ int synch_destroy(pthread_mutex_t* lock) {
     return 0;
 }
 
-/*
-
-// HATCH
-
-hatch nodelist func(arg1, arg2, arg3, arg4....) {
-
-}
-
-nodelist func(node current, node goal) {
-    print(current.data)
-}
-// iSSUE: DO WE ENFORCE THAT FIRST ARG IS A NODE?
-// HOW ELSE CAN THIS BE DONE??
-
-
-pthread_t* arr; 
-
-int hatch_start(int len, HOW_TO_PASS_FUNC, HOW_TO_PASS_ARGS, NODELIST?) {
-    pthread_t* pthreadArray[len];
-    arr = pthreadArray;
-    // 1st. empty array
-    // put threads in here
-    // make array global variable, length is length of nodelist
-
-    //  forloop where we make pthreads
-    int i;
-    for (i = 0; i < len; i++) {
-        err = pthread_create(&(arr[i]), NULL, &func, NEED_ARGS, NODE_IN_NODELIST?);
-        if (err != 0) {
-            printf("\ncan't create thread :[%s]", strerror(err));
-            return -1;
-        } 
-    }
-    // the function associated with them is func
-    // func is called on the args within it and then node at the end from nodelist
-} */
-
-
-
 int hatch_end(pthread_t* thread_arr, int len) {
     int j;
     for (j = 0; j < len; j++) {
