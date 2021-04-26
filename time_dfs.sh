7fca034370c0dd6a78c496b6a8329947e33a68bd
pelincetin <pc2807@barnard.edu>
2021-04-26 02:34:30 -0400

    updated timer

--- time_dfs.sh
+++ time_dfs.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
-make clean
-make
 
-# get the time in miliseconds for normaldfs
-ts=$((`date '+%s%N'`/1000000));
-./scripts/testone.sh dfs;
+#make clean
+#make
 
-tt=$((`date '+%s%N'`/1000000));
+for i in {1..10}
+    # get the time in miliseconds for normaldfs
+    do ts="$((`date '+%s%N'`/1000000))";
 
-# get the time in miliseconds for conc dfs
-conc_ts=$((`date '+%s%N'`/1000000));
-./scripts/testone.sh concdfs; 
+    ./scripts/testone.sh dfs;
 
-conc_tt=$((`date '+%s%N'`/1000000)); 
+    tt="$((`date '+%s%N'`/1000000))";
+    diff=`expr $tt - $ts`;
+    a=`expr $a + $diff`;
+
+    # get the time in miliseconds for conc dfs
+    conc_ts="$((`date '+%s%N'`/1000000))";
+    ./scripts/testone.sh concdfs; 
+
+    conc_tt="$((`date '+%s%N'`/1000000))"; 
+    diff=`expr $conc_tt - $conc_ts`;
+    b=`expr $b + $diff`;
+done
+
 
 # echo the difference between normaldfs and concdfs
-echo "Time taken to run normaldfs: "$(($tt-$ts))" milliseconds";
-echo "Time taken to run concdfs: "$(($conc_tt-$conc_ts))" milliseconds";
+echo "Ran the tests ten times. Here are the results: ";
+echo "Time taken to run normaldfs ten times is: "$a" milliseconds";
+echo "Time taken to run concdfs ten times is: "$b" milliseconds";
+res1=`expr $a / 10`;
+res2=`expr $b / 10`;
+echo "On average, normaldfs ran in: "$res1" milliseconds";
+echo "On average, concdfs ran in: "$res2" milliseconds";
